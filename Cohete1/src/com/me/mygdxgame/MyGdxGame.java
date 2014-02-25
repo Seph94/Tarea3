e6105b6ed1c8315b2ba846c3687c2b7c43971f7e
Alumno <alumno@localhost.localdomain>
2014-02-25 12:41:03 -0600

    Cohete

---
+++ Cohete1/src/com/me/mygdxgame/MyGdxGame.java
@@ -0,0 +1,88 @@
+package com.me.mygdxgame;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class MyGdxGame implements ApplicationListener {
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture texture;
+	private Texture texture_fondo;
+	private Texture texture_nave;
+	private Sprite sprite;
+	private Sprite sprite_nave;
+	private   Sprite sprite_fondo;
+
+
+	
+	@Override
+	public void create() {		
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		
+		camera = new OrthographicCamera(1, h/w);
+		batch = new SpriteBatch();
+		
+		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
+		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		
+		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+		
+		sprite = new Sprite(region);
+		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		
+
+		texture_nave= new Texture(Gdx.files.internal("data/nave.png"));
+		sprite_nave=new Sprite(texture_nave,128,64);
+		sprite_nave.setPosition(-0.1f, -0.1f);
+		sprite_nave.setSize(0.2f,0.2f);
+
+
+		texture_fondo= new Texture(Gdx.files.internal("data/fondo.png"));
+		sprite_fondo=new Sprite(texture_fondo,128,64);
+		sprite_fondo.setPosition(-0.5f, -0.1f);
+		sprite_fondo.setSize(0.4f,0.4f);
+		
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		texture.dispose();
+	}
+
+	@Override
+	public void render() {		
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		sprite_nave.draw(batch);
+		sprite_fondo.draw(batch);
+		
+		batch.end();
+		
+	}
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
+
